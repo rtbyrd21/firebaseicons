890b72978568416535441e5ea687906f8b37500c
Rob Byrd <byrd.t.rob@gmail.com>
2015-01-22 23:45:37 -0500

    first commit

---
+++ builds/development/js/services/authentication.js
@@ -0,0 +1,38 @@
+myApp.factory('Authentication', function($firebase, 
+  $firebaseAuth, FIREBASE_URL, $location, $rootScope) {
+
+  var ref = new Firebase(FIREBASE_URL);
+    var simpleLogin = $firebaseAuth(ref);
+    //Predefine the factory
+    var factory = {
+       login: login,
+       authorized: null
+//       signedIn: signedIn
+    };
+
+    function login() {
+       return simpleLogin.$authAnonymously().then(function(authData) {
+          var userref = new Firebase(FIREBASE_URL + 'users');
+          var firebaseUsers = $firebase(userref);
+         
+         var ref = new Firebase(FIREBASE_URL + '/users/' + authData.uid);
+  var userObj = $firebase(ref).$asObject();
+         
+         userObj.$loaded().then(function(){
+          $rootScope.currentUser = userObj;
+         })
+         
+          
+         var userInfo = {
+            regUser: authData.uid
+         }
+         firebaseUsers.$set(authData.uid, userInfo);
+          factory.authorized = authData.uid; //Set the property here
+         $location.path('/meetings');
+      }).catch(function(error) {});
+    } 
+  
+ 
+   //return it
+   return factory;
+});
